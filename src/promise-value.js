334592b17f49ef987ad1d05961b0aadb999b3871
Daniel Winterstein <daniel@winterwell.com>
2017-08-28 11:44:42 +0100

    simples

--- src/promise-value.js
+++ src/promise-value.js
@@ -1,61 +1,35 @@
+
 /**
- * Make a bag of string constants, kind of like a Java enum.
- * e.g. var MyKind = new Enum('TEXT PERSON');
- * gives you MyKind.TEXT == 'TEXT', MyKind.PERSON == 'PERSON'
- *
- * Also, each of the constants has an isCONSTANT() function added, so you can write:
- * MyKind.isTEXT(myvar) -- which has the advantage that it will create a noisy error if
- * if myvar is invalid (e.g. it helps catch typos). isCONSTANT() allows falsy values, but an unrecognised
- * non-false value indicates an error.
- *
- * MyKind.values holds the full list (and is frozen to keep it safe from edits).
- *
- * MyKind.has(thing) provides a test, true if thing is a valid value.
  * 
- * Use-case: It's safer than just using strings for constants, especially around refactoring.
- * It does use strings, because you want to work with json.
- *
- * @author Daniel
- * Ref: http://stijndewitt.wordpress.com/2014/01/26/enums-in-javascript/
+ * @param {*} valueOrPromise 
+ * @returns {value: ?Object, promise: !Promise} The return is never null, and the promise part is always set.
+ * 	The behaviour depends on valueOrPromise:
+ * 	If it's a value -> resolved Promise
+ * * 	If it's a Promise (or thenable) -> the input Promise
+ * 	null/undefined -> rejected Promise
  */
-class Enum {
-
-	/** @param values {string|string[]}
-	*/
-	constructor(values) {
-		// Set the values array
-		if (typeof(values)==='string') {
-			this.values = values.split(' ');
-		} else {
-			this.values = values;
-		}
-		for(let i=0; i<this.values.length; i++) {
-			let k = this.values[i];
-			this[k] = k;
-			/** isCONSTANT: {string} -> {boolean} */
-			this['is'+k] = function(v) {
-				if ( ! v) return false;
-				if ( ! this.enumerator[v]) throw new Error('Invalid Enum value: '+v);
-				return v===this.k;
-			}.bind({enumerator:this, k:k});
-			/** $CONSTANT: {string} -> {string} safety accessor */
-			this['$'+k] = () => k;
-		}		
-		// Prevent edits
-		if (Object.freeze) {
-			Object.freeze(this);
-			Object.freeze(this.values);
-		}
+function pv(valueOrPromise) {
+	if (valueOrPromise===null || valueOrPromise===undefined) {
+		return {
+			promise: Promise.fail(new Error("null value"))
+		};
 	}
+	// NB: Promise.resolve() can be used with Promises without nesting	
+	if (typeof(valueOrPromise.then) === 'function') {		
+		// Having then() is the only real requirement for a Promise
+		const vp = {promise:valueOrPromise};
+		// set the value when we have it
+		valueOrPromise.then(r => {
+			vp.value = r;
+			return r;
+		});
+		return vp;	
+	}
+	// It's a value - return now
+	return {
+		value: valueOrPromise,
+		promise: Promise.resolve(valueOrPromise)
+	};
+};
 
-	/**
-	 * @param s {string}
-	 * @returns true if s is a value of this enum, false otherwise.
-	 * Use-case: assert(has(someInput));
-	 */
-	has(s) {
-		return this.values.indexOf(s) != -1;
-	}	
-}
-
-export default Enum;
+export default pv;
--- test/test.promise-value.js
+++ test/test.promise-value.js
@@ -1,21 +1,11 @@
 
-SJTest.run({name:'Enum',
-
-	simpleTest: function() {
-		let FRUIT = new Enum("APPLE PEAR");
-		assert(FRUIT.APPLE);
-		assert(FRUIT.APPLE === 'APPLE');
-		assert( ! FRUIT.CHAIR);
-		assert(FRUIT.isAPPLE('APPLE'));
-		assert(FRUIT.has('APPLE'));
-		assert( ! FRUIT.has('legs'));
-	},
-
-	callTest: function() {
-		// is there a way to make FRUIT.value safe against typos etc?
-		let FRUIT = new Enum("APPLE PEAR");
-		assert(FRUIT.$APPLE() === 'APPLE');
-		FRUIT.$cat();
+SJTest.run({name:'PromiseValue: value',
+	
+	function() {
+		let a = pv(7);
+		assert(a.value===7, a);
+		assert(a.promise.then);
+		return "OK";
 	}
 
 });
