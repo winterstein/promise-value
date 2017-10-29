08f7aae2310d7b6c92bef62faaa8b17a56a4edd2
Daniel Winterstein <daniel@sodash.com>
2017-10-29 09:48:25 +0000

    with build

--- test/test.promise-value.js
+++ test/test.promise-value.js
@@ -1,22 +1,26 @@
 
-SJTest.run({name:'PromiseValue: value',
-	
+SJTest.run({'PromiseValue: value':
 	function() {
 		let a = pv(7);
 		assert(a.value===7, a);
 		assert(a.promise.then);
 		return "OK";
 	}
-
 });
 
-SJTest.run({name:'PromiseValue: reject (fail)',
+SJTest.run({'PromiseValue: reject (fail)':
+	function() {
+		let a = pv(undefined);
+		assert( ! a.value, a);
+		assert(a.promise.then);
+		return "OK";
+	}
+});
 
-function() {
-	let a = pv(undefined);
-	assert( ! a.value, a);
-	assert(a.promise.then);
-	return "OK";
-}
-
+SJTest.run({
+	'PromiseValue: resolved':
+	function() {
+		let a = pv("123");
+		assert(a.resolved, a);
+	}
 });
